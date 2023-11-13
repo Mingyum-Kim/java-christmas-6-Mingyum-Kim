8132f77b6c651018e5354d6f716b0fb87bf0e8db
MingyeomKim <mingyum119@naver.com>
2023-11-13 20:46:56 +0900

    feat(InputView): 예상 방문 날짜 입력을 요청하는 메시지 출력

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,11 @@
+package christmas.view;
+
+import christmas.view.console.ConsoleWriter;
+
+public class InputView {
+    private static final String DATE_REQUEST_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+
+    public void requestDate() {
+        ConsoleWriter.printlnMessage(DATE_REQUEST_MESSAGE);
+    }
+}
