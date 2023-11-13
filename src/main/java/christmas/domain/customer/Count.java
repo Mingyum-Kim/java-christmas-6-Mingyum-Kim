31354691f1f11159fe86e0cde2a25ea0aec89191
MingyeomKim <mingyum119@naver.com>
2023-11-14 02:33:05 +0900

    feat(Count): 메뉴의 개수가 1 이상임을 검증하는 로직 구현
    메뉴 하나가 주문된 갯수를 저장하는 도메인 Count에서 개수가 1 이상임을 검증하는 로직을 구현한다.

--- src/main/java/christmas/domain/customer/Count.java
+++ src/main/java/christmas/domain/customer/Count.java
@@ -1,13 +1,33 @@
 package christmas.domain.customer;
 
+import christmas.global.exception.CustomException;
+import christmas.global.exception.ErrorMessage;
+
 public class Count {
     private final int count;
 
     private Count(int count) {
-        this.count = count;
+        this.count = Validator.validate(count);
     }
 
     public static Count from(int count) {
         return new Count(count);
+    }
+
+    private static class Validator {
+        private static int validate(int count) {
+            validatePositive(count);
+            return count;
+        }
+
+        private static void validatePositive(int count) {
+            if (isNotPositive(count)) {
+                throw CustomException.from(ErrorMessage.INVALID_ORDER_ERROR);
+            }
+        }
+
+        private static boolean isNotPositive(int count) {
+            return count < 1;
+        }
     }
 }
