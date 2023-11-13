ebf4d626deb11f61408ac02c70c2efc5adf11811
MingyeomKim <mingyum119@naver.com>
2023-11-14 04:42:48 +0900

    feat(OutputView): 주문 메뉴 안내 메세지를 출력

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -6,6 +6,7 @@ import christmas.view.console.ConsoleWriter;
 public class OutputView {
     private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String PROMOTION_NOTICE_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    private static final String ORDER_RESPONSE_MESSAGE = "<주문 메뉴>";
 
     public void start() {
         ConsoleWriter.printlnMessage(START_MESSAGE);
@@ -13,5 +14,6 @@ public class OutputView {
 
     public void printOrders(CustomerResponse customerResponse) {
         ConsoleWriter.printlnFormat(PROMOTION_NOTICE_MESSAGE, customerResponse.date());
+        ConsoleWriter.printlnMessage(ORDER_RESPONSE_MESSAGE);
     }
 }
