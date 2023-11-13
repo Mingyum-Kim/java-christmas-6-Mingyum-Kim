b29f82de70c72c8969618c613bba481c33555b08
MingyeomKim <mingyum119@naver.com>
2023-11-13 21:34:02 +0900

    feat(Date): 날짜 입력이 1 이상 31 이하의 숫자임을 검증
    날짜를 저장하는 도메인 클래스에서 입력된 날짜가 1 이상 31 이하의 숫자에 해당함을 검증한다.

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -1,13 +1,37 @@
 package christmas.domain;
 
+import christmas.global.exception.CustomException;
+import christmas.global.exception.ErrorMessage;
+
 public class Date {
+    private static final int START_DATE = 1;
+    private static final int END_DATE = 31;
+
     private final int date;
 
     private Date(int date) {
+        Validator.validate(date);
         this.date = date;
     }
 
     public static Date from(int date) {
         return new Date(date);
+    }
+
+    private static class Validator {
+        private static int validate(int date) {
+            validateRange(date);
+            return date;
+        }
+
+        private static void validateRange(int date) {
+            if (isInvalidRange(date)) {
+                throw CustomException.from(ErrorMessage.INVALID_DATE_ERROR);
+            }
+        }
+
+        private static boolean isInvalidRange(int date) {
+            return date < START_DATE || date > END_DATE;
+        }
     }
 }
