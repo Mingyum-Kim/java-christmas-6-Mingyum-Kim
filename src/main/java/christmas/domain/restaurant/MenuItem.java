c27b2f353bcbe86a093b72b4bff9075898a7dbd2
MingyeomKim <mingyum119@naver.com>
2023-11-14 02:46:47 +0900

    feat(MenuItem): 메뉴의 한글 이름을 저장하는 인스턴스 변수 추가
    한글 이름으로 MenuItem 객체를 조회하기 위해 한글 이름을 저장하는 인스턴스 변수를 추가한다.

--- src/main/java/christmas/domain/restaurant/MenuItem.java
+++ src/main/java/christmas/domain/restaurant/MenuItem.java
@@ -1,29 +1,27 @@
 package christmas.domain.restaurant;
 
 public enum MenuItem {
-    MUSHROOM_SOUP(6_000),
-    TAPAS(5_500),
-    CAESAR_SALAD(8_000),
+    MUSHROOM_SOUP("양송이수프", 6_000),
+    TAPAS("타파스", 5_500),
+    CAESAR_SALAD("시저샐러드", 8_000),
 
-    TBONE_STEAK(55_000),
-    BARBECUE_RIBS(54_000),
-    SEAFOOD_PASTA(35_000),
-    CHRISTMAS_PASTA(25_000),
+    TBONE_STEAK("티본스테이크", 55_000),
+    BARBECUE_RIBS("바비큐립", 54_000),
+    SEAFOOD_PASTA("해산물파스타", 35_000),
+    CHRISTMAS_PASTA("크리스마스파스타", 25_000),
 
-    CHOCOLATE_CAKE(15_000),
-    ICE_CREAM(5_000),
+    CHOCOLATE_CAKE("초코케이크", 15_000),
+    ICE_CREAM("아이스크림", 5_000),
 
-    ZERO_COLA(3_000),
-    RED_WINE(60_000),
-    CHAMPAGNE(25_000);
+    ZERO_COLA("제로콜라", 3_000),
+    RED_WINE("레드와인", 60_000),
+    CHAMPAGNE("샴페인", 25_000);
 
+    private String name;
     private int price;
 
-    MenuItem(int price) {
+    MenuItem(String name, int price) {
+        this.name = name;
         this.price = price;
-    }
-
-    public int getPrice() {
-        return price;
     }
 }
