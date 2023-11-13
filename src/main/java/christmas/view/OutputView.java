583129cd9294617ead6258d47c7e0ce2f30fe9fc
MingyeomKim <mingyum119@naver.com>
2023-11-14 04:40:45 +0900

    feat(OutputView): 이벤트 혜택을 안내하는 메세지를 출력

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,16 +1,17 @@
 package christmas.view;
 
-import christmas.controller.dto.response.OrdersResponse;
+import christmas.controller.dto.response.CustomerResponse;
 import christmas.view.console.ConsoleWriter;
 
 public class OutputView {
     private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String PROMOTION_NOTICE_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
 
     public void start() {
         ConsoleWriter.printlnMessage(START_MESSAGE);
     }
 
-    public void printOrders(OrdersResponse ordersResponse) {
-
+    public void printOrders(CustomerResponse customerResponse) {
+        ConsoleWriter.printlnFormat(PROMOTION_NOTICE_MESSAGE, customerResponse.date());
     }
 }
--- src/main/java/christmas/view/console/ConsoleWriter.java
+++ src/main/java/christmas/view/console/ConsoleWriter.java
@@ -4,4 +4,8 @@ public final class ConsoleWriter {
     public static void printlnMessage(String message) {
         System.out.println(message);
     }
+
+    public static void printlnFormat(String message, Object... args) {
+        printlnMessage(String.format(message, args));
+    }
 }
