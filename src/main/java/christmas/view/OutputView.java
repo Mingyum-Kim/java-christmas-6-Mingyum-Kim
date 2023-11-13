122c053a22db0b51712475e677a7bbc8c9cdf85e
MingyeomKim <mingyum119@naver.com>
2023-11-14 05:03:14 +0900

    feat(OutputView): 할인 전 총주문금액 안내 메세지 출력

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -8,6 +8,8 @@ public class OutputView {
     private static final String PROMOTION_NOTICE_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String ORDER_NOTICE_MESSAGE = "\n<주문 메뉴>";
     private static final String ORDER_RESPONSE_MESSAGE = "%s %d개";
+    private static final String TOTAL_COST_NOTICE_MESSAGE = "\n<할인 전 총주문 금액>";
+    private static final String COST_MESSAGE = "%,d원";
 
     public void start() {
         ConsoleWriter.printlnMessage(START_MESSAGE);
@@ -23,5 +25,9 @@ public class OutputView {
                         order.name(),
                         order.count())
                 );
+    }
+
+    public void printTotalCost(final int calculateTotalCost) {
+        ConsoleWriter.printlnMessage(TOTAL_COST_NOTICE_MESSAGE);
     }
 }
