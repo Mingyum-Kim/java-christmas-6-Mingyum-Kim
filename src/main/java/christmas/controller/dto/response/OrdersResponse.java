4ba491f121a0d84216fc838203bd53dec62f2f29
MingyeomKim <mingyum119@naver.com>
2023-11-14 04:07:26 +0900

    refactor(OrdersResponse): OrdersResponse를 record로 지정
    DTO 클래스를 간단하게 개발하기 위해 레코드를 사용한다.

--- src/main/java/christmas/controller/dto/response/OrdersResponse.java
+++ src/main/java/christmas/controller/dto/response/OrdersResponse.java
@@ -2,6 +2,7 @@ package christmas.controller.dto.response;
 
 import java.util.List;
 
-public class OrdersResponse {
-    List<OrderResponse> orders;
+public record OrdersResponse(
+        List<OrderResponse> orders
+) {
 }
