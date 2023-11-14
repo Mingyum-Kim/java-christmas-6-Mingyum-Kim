ad7ff72744fee82688b4a4511c5d3592188fef1a
MingyeomKim <mingyum119@naver.com>
2023-11-15 04:29:25 +0900

    feat(BenefitResponse): 이벤트 이름과 혜택 금액을 저장하는 DTO 클래스 생성
    Benefit 클래스의 하위 타입에 따라 서로 다른 혜택을 계산하고 있으므로, 분기처리하여 뷰 레이어에 전달할 DTO를 생성한다.

--- src/main/java/christmas/controller/dto/response/BenefitResponse.java
+++ src/main/java/christmas/controller/dto/response/BenefitResponse.java
@@ -1,7 +1,28 @@
 package christmas.controller.dto.response;
 
+import christmas.domain.promotion.Benefit;
+import christmas.domain.promotion.Discount;
+import christmas.domain.promotion.Gift;
+import christmas.domain.promotion.Promotion;
+
 public record BenefitResponse(
         String promotion,
         int price
 ) {
+    public static BenefitResponse of(Promotion promotion, Benefit benefit) {
+        if (benefit instanceof Discount) {
+            return new BenefitResponse(
+                    promotion.getName(),
+                    cast(benefit, Discount.class).getPrice()
+            );
+        }
+        return new BenefitResponse(
+                promotion.getName(),
+                cast(benefit, Gift.class).getPrice()
+        );
+    }
+
+    private static <T> T cast(Benefit benefit, Class<T> clazz) {
+        return clazz.cast(benefit);
+    }
 }
