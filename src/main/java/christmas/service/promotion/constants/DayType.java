a773e30ec146df1cdd233f4e3370d7fd545ca0b9
MingyeomKim <mingyum119@naver.com>
2023-11-14 13:25:52 +0900

    feat(DayType): 평일과 주말을 나누어 요일 상수를 그룹화
    DayType에서 평일인 요일들과 주말인 요일들을 나누어 리스트로 저장하고, 평일과 주말 정보를 상수화한다.

---
+++ src/main/java/christmas/service/promotion/constants/DayType.java
@@ -0,0 +1,24 @@
+package christmas.service.promotion.constants;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum DayType {
+    WEEKDAY(Arrays.asList(
+            DayOfWeek.SUNDAY,
+            DayOfWeek.MONDAY,
+            DayOfWeek.TUESDAY,
+            DayOfWeek.WEDNESDAY,
+            DayOfWeek.THURSDAY
+    )),
+    WEEKEND(Arrays.asList(
+            DayOfWeek.FRIDAY,
+            DayOfWeek.SATURDAY
+    ));
+
+    private final List<DayOfWeek> days;
+
+    DayType(List<DayOfWeek> days) {
+        this.days = days;
+    }
+}
