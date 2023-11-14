4cc80d471b32e40712260836aa8dbd0f039ce313
MingyeomKim <mingyum119@naver.com>
2023-11-15 02:18:02 +0900

    feat(OutputView): 증정 메뉴의 이름과 개수를 출력하는 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,14 +1,14 @@
 package christmas.view;
 
 import christmas.controller.dto.response.CustomerResponse;
-import christmas.controller.dto.response.PromotionsResponse;
+import christmas.controller.dto.response.GiftsResponse;
 import christmas.view.console.ConsoleWriter;
 
 public class OutputView {
     private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String PROMOTION_NOTICE_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String ORDER_NOTICE_MESSAGE = "\n<주문 메뉴>";
-    private static final String ORDER_RESPONSE_MESSAGE = "%s %d개";
+    private static final String MENU_RESPONSE_MESSAGE = "%s %d개";
     private static final String TOTAL_COST_NOTICE_MESSAGE = "\n<할인 전 총주문 금액>";
     private static final String COST_MESSAGE = "%,d원";
     private static final String GIFT_MENU_NOTICE_MESSAGE = "\n<증정 메뉴>";
@@ -23,7 +23,7 @@ public class OutputView {
 
         customerResponse.orders()
                 .forEach(order -> ConsoleWriter.printlnFormat(
-                        ORDER_RESPONSE_MESSAGE,
+                        MENU_RESPONSE_MESSAGE,
                         order.name(),
                         order.count())
                 );
@@ -34,7 +34,16 @@ public class OutputView {
         ConsoleWriter.printlnFormat(COST_MESSAGE, totalCost);
     }
 
-    public void printGiftMenu(PromotionsResponse response) {
+    public void printGiftMenu(GiftsResponse giftsResponse) {
         ConsoleWriter.printlnMessage(GIFT_MENU_NOTICE_MESSAGE);
+        giftsResponse.responses()
+                .forEach(response -> printGiftMenu(
+                        response.name(),
+                        response.count())
+                );
+    }
+
+    private void printGiftMenu(String name, int count) {
+        ConsoleWriter.printlnFormat(GIFT_MENU_NOTICE_MESSAGE, name, count);
     }
 }
