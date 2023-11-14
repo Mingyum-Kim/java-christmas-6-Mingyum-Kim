b49ff71e1c5a5e8ec082325de7ebb1c2e82fa2c9
MingyeomKim <mingyum119@naver.com>
2023-11-14 21:57:48 +0900

    feat(SpecialPromotion): 방문 날짜가 특별 할인 이벤트 적용 날짜와 같은지 확인
    구현 클래스 내에 int형 배열을 선언하여  특별 할인 이벤트를 적용하는 날짜임을 저장한다.

---
+++ src/main/java/christmas/service/promotion/SpecialPromotion.java
@@ -0,0 +1,24 @@
+package christmas.service.promotion;
+
+import christmas.domain.customer.Date;
+import christmas.domain.customer.Orders;
+import christmas.domain.promotion.Discount;
+import java.util.Arrays;
+
+public class SpecialPromotion implements PromotionService<Discount> {
+    private static final int[] specialDays = {
+            3, 10, 17, 24, 25, 31
+    };
+
+    @Override
+    public Discount apply(Date date, Orders orders) {
+        if (isQualified(date)) {
+
+        }
+    }
+
+    private boolean isQualified(Date date) {
+        return Arrays.stream(specialDays)
+                .anyMatch(day -> date.equals(day));
+    }
+}
