79b9793899d909f576c2e50f8158d8d88f489a69
MingyeomKim <mingyum119@naver.com>
2023-11-14 21:59:38 +0900

    feat(SpecialPromotion): 특별 할인 이벤트를 적용하고 할인 금액을 반환
    특별 할인 이벤트의 할인 가격을 상수로 정의하고 조건에 부합할 시 반환한다.

--- src/main/java/christmas/service/promotion/SpecialPromotion.java
+++ src/main/java/christmas/service/promotion/SpecialPromotion.java
@@ -9,12 +9,14 @@ public class SpecialPromotion implements PromotionService<Discount> {
     private static final int[] specialDays = {
             3, 10, 17, 24, 25, 31
     };
+    private static final int DISCOUNT_PRICE = 1000;
 
     @Override
     public Discount apply(Date date, Orders orders) {
         if (isQualified(date)) {
-
+            return Discount.from(DISCOUNT_PRICE);
         }
+        return Discount.from(0);
     }
 
     private boolean isQualified(Date date) {
