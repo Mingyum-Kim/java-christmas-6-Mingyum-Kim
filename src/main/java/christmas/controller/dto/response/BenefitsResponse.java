017447354858e0a28c45a0884f0ac7bf4edabad1
MingyeomKim <mingyum119@naver.com>
2023-11-15 04:27:49 +0900

    feat(BenefitsResponse): 혜택 내역 객체를 출력하기 위한 DTO 클래스 생성
    `PromotionsResponse`에서 뷰 레이어에 전달하기 위한 DTO 클래스를 생성하는 메서드를 구현한다.

--- src/main/java/christmas/controller/dto/response/BenefitsResponse.java
+++ src/main/java/christmas/controller/dto/response/BenefitsResponse.java
@@ -1,8 +1,24 @@
 package christmas.controller.dto.response;
 
+import christmas.service.dto.response.PromotionResponse;
+import christmas.service.dto.response.PromotionsResponse;
+import java.util.ArrayList;
 import java.util.List;
 
 public record BenefitsResponse(
         List<BenefitResponse> responses
 ) {
+    public static BenefitsResponse from(PromotionsResponse promotionsResponse) {
+        List<BenefitResponse> benefitResponses = new ArrayList<>();
+
+        List<PromotionResponse> responses = promotionsResponse.responses();
+        for (PromotionResponse response : responses) {
+            BenefitResponse benefitResponse = BenefitResponse.of(
+                    response.promotion(),
+                    response.benefit()
+            );
+            benefitResponses.add(benefitResponse);
+        }
+        return new BenefitsResponse(benefitResponses);
+    }
 }
