00bbe46d81203a528e27816fbd64e52385a625ce
MingyeomKim <mingyum119@naver.com>
2023-11-15 04:55:45 +0900

    feat(EventPlanner): 할인 전 총 주문 금액에서 할인 금액을 뺀 결제 금액을 계산

--- src/main/java/christmas/controller/EventPlanner.java
+++ src/main/java/christmas/controller/EventPlanner.java
@@ -1,21 +1,27 @@
 package christmas.controller;
 
-import christmas.controller.dto.response.BenefitsResponse;
-import christmas.controller.dto.response.CustomerResponse;
-import christmas.controller.dto.response.GiftsResponse;
+import christmas.controller.dto.response.benefit.BenefitsResponse;
+import christmas.controller.dto.response.gift.GiftsResponse;
+import christmas.controller.dto.response.order.CustomerResponse;
 import christmas.domain.customer.Date;
 import christmas.domain.customer.Orders;
+import christmas.domain.promotion.Discount;
+import christmas.service.dto.response.PromotionResponse;
 import christmas.service.dto.response.PromotionsResponse;
 import christmas.service.promotion.PromotionHandler;
 import christmas.view.InputView;
 import christmas.view.OutputView;
+import java.util.List;
 
 public class EventPlanner {
     private final InputView inputView;
     private final OutputView outputView;
     private final PromotionHandler promotionHandler;
 
-    public EventPlanner(InputView inputView, OutputView outputView, PromotionHandler promotionHandler) {
+    public EventPlanner(InputView inputView,
+                        OutputView outputView,
+                        PromotionHandler promotionHandler
+    ) {
         this.inputView = inputView;
         this.outputView = outputView;
         this.promotionHandler = promotionHandler;
@@ -30,11 +36,26 @@ public class EventPlanner {
     }
 
 
-    private void response(Date date, Orders orders, PromotionsResponse promotionsResponse) {
+    private void response(Date date,
+                          Orders orders,
+                          PromotionsResponse promotionsResponse
+    ) {
         outputView.printOrders(new CustomerResponse(date.getDate(), orders.toResponse()));
         outputView.printTotalCost(orders.calculateOrdersCost());
 
         outputView.printGiftMenu(GiftsResponse.from(promotionsResponse));
         outputView.printBenefits(BenefitsResponse.from(promotionsResponse));
+        outputView.printCost(calculateCost(orders, promotionsResponse));
+    }
+
+    private int calculateCost(Orders orders, PromotionsResponse promotionsResponse) {
+        return orders.calculateOrdersCost() - calculateDiscount(promotionsResponse.responses());
+    }
+
+    private int calculateDiscount(List<PromotionResponse> promotionResponses) {
+        return promotionResponses.stream()
+                .filter(response -> response.benefit() instanceof Discount)
+                .mapToInt(response -> ((Discount) response.benefit()).getPrice())
+                .sum();
     }
 }
