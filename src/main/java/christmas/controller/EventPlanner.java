fda2c629864d8dc8fe3e162ca20058d39ddae937
MingyeomKim <mingyum119@naver.com>
2023-11-15 02:07:18 +0900

    feat(EventPlanner): Benefit 객체를 다운 캐스팅하여 `GiftResponse`로 변환하는 로직 구현
    PromotionResponse의 benefit 타입이 Gift인 경우 다운 캐스팅하여 GiftsResponse를 생성하는 로직 구현

--- src/main/java/christmas/controller/EventPlanner.java
+++ src/main/java/christmas/controller/EventPlanner.java
@@ -2,9 +2,15 @@ package christmas.controller;
 
 import christmas.controller.dto.request.OrdersRequest;
 import christmas.controller.dto.response.CustomerResponse;
+import christmas.controller.dto.response.GiftResponse;
+import christmas.controller.dto.response.GiftsResponse;
 import christmas.domain.customer.Date;
 import christmas.domain.customer.Order;
 import christmas.domain.customer.Orders;
+import christmas.domain.promotion.Gift;
+import christmas.service.dto.response.PromotionResponse;
+import christmas.service.dto.response.PromotionsResponse;
+import christmas.service.promotion.PromotionHandler;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 import java.util.List;
@@ -12,28 +18,47 @@ import java.util.List;
 public class EventPlanner {
     private final InputView inputView;
     private final OutputView outputView;
+    private final PromotionHandler promotionHandler;
 
-    public EventPlanner(InputView inputView, OutputView outputView) {
+    public EventPlanner(InputView inputView, OutputView outputView, PromotionHandler promotionHandler) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.promotionHandler = promotionHandler;
         outputView.start();
     }
 
     public void run() {
         Date date = Date.from(inputView.requestDate());
-        Orders orders = Orders.from(convertToOrders(inputView.requestOrders()));
-        response(date, orders);
+        Orders orders = Orders.from(convertOrders(inputView.requestOrders()));
+
+        PromotionsResponse response = promotionHandler.process(date, orders);
+        response(date, orders, response);
     }
 
-    private List<Order> convertToOrders(OrdersRequest ordersRequest) {
+    private List<Order> convertOrders(OrdersRequest ordersRequest) {
         return ordersRequest.orders()
                 .stream()
                 .map(order -> Order.of(order.name(), order.count()))
                 .toList();
     }
 
-    private void response(Date date, Orders orders) {
+    private void response(Date date, Orders orders, PromotionsResponse promotionsResponse) {
         outputView.printOrders(new CustomerResponse(date.getDate(), orders.toResponse()));
         outputView.printTotalCost(orders.calculateOrdersCost());
+
+        outputView.printGiftMenu(toGiftsResponse(promotionsResponse));
+    }
+
+    private GiftsResponse toGiftsResponse(PromotionsResponse promotionsResponse) {
+        List<GiftResponse> giftResponses = toGiftResponses(promotionsResponse.responses());
+        return new GiftsResponse(giftResponses);
+    }
+
+    private List<GiftResponse> toGiftResponses(List<PromotionResponse> promotionResponses) {
+        return promotionResponses.stream()
+                .filter(response -> response.benefit() instanceof Gift)
+                .map(response -> (Gift) response.benefit())
+                .map(Gift::toResponse)
+                .toList();
     }
 }
