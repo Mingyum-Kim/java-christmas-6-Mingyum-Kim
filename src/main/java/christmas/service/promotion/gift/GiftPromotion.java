730d23588cf14032bb181de486394b6619d75ea1
MingyeomKim <mingyum119@naver.com>
2023-11-14 23:53:24 +0900

    refactor(GiftPromotion): 증정 이벤트의 결과로 이벤트 타입을 함께 반환하도록 수정
    생성한 DTO 클래스를 활용해 이벤트 결과로 이벤트의 타입과 함께 결괏값이 반환되도록 설정한다.

--- src/main/java/christmas/service/promotion/gift/GiftPromotion.java
+++ src/main/java/christmas/service/promotion/gift/GiftPromotion.java
@@ -1,23 +1,31 @@
 package christmas.service.promotion.gift;
 
+import christmas.controller.dto.response.PromotionResponse;
 import christmas.domain.customer.Count;
 import christmas.domain.customer.Date;
 import christmas.domain.customer.Orders;
 import christmas.domain.promotion.Gift;
+import christmas.domain.promotion.Promotion;
 import christmas.domain.restaurant.MenuItem;
 import christmas.service.promotion.PromotionService;
 
-public class GiftPromotion implements PromotionService<Gift> {
+public class GiftPromotion implements PromotionService<PromotionResponse<Gift>> {
     private static final MenuItem GIFT_ITEM = MenuItem.CHAMPAGNE;
     private static final int GIFT_COUNT = 1;
     private static final int THRESHOLD = 120_000;
 
     @Override
-    public Gift apply(Date date, Orders orders) {
+    public PromotionResponse<Gift> apply(Date date, Orders orders) {
         if (isQualified(orders)) {
-            return Gift.of(GIFT_ITEM, Count.valueOf(GIFT_COUNT));
+            return new PromotionResponse(
+                    Promotion.GIFT,
+                    Gift.of(GIFT_ITEM, Count.valueOf(GIFT_COUNT))
+            );
         }
-        return Gift.of(MenuItem.NONE, Count.valueOf(0));
+        return new PromotionResponse(
+                Promotion.GIFT,
+                Gift.of(MenuItem.NONE, Count.valueOf(0))
+        );
     }
 
     private boolean isQualified(Orders orders) {
