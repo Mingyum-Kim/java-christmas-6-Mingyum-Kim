0b5c232ff5be97c2bcd8ffff7adeecaec90a8cac
MingyeomKim <mingyum119@naver.com>
2023-11-15 14:20:44 +0900

    refactor(Menu): 메뉴 아이템을 조회하는 로직 수정
    메뉴의 리스트를 순회하는 함수를 제거하고 `contains` 메서드로 구현한다.

--- src/main/java/christmas/domain/restaurant/Menu.java
+++ src/main/java/christmas/domain/restaurant/Menu.java
@@ -32,19 +32,21 @@ public enum Menu {
         this.menuItems = menuItems;
     }
 
+    /**
+     * 메뉴 아이템이 특정 메뉴 분류에 확인하는 메서드
+     *
+     * @param menu     메뉴 분류
+     * @param menuItem 메뉴 아이템
+     * @return 메뉴 분류에 속하는 메뉴 아이템이면 true, 그렇지 않으면 false
+     */
     public static boolean contains(Menu menu, MenuItem menuItem) {
         return findByMenuItem(menuItem).equals(menu);
     }
 
-    public static Menu findByMenuItem(MenuItem menuItem) {
-        return Arrays.stream(Menu.values())
-                .filter(menu -> menu.hasMenuItem(menuItem))
-                .findAny()
+    private static Menu findByMenuItem(MenuItem menuItem) {
+        return Arrays.stream(values())
+                .filter(menu -> menu.menuItems.contains(menuItem))
+                .findFirst()
                 .orElse(EMPTY);
-    }
-
-    private boolean hasMenuItem(MenuItem target) {
-        return menuItems.stream()
-                .anyMatch(menItem -> menuItems.equals(target));
     }
 }
