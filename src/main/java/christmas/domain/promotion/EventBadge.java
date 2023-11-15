c3c82492a2ff399c46686964a6093a4e11c818e4
MingyeomKim <mingyum119@naver.com>
2023-11-15 13:25:34 +0900

    feat(EventBadge): 혜택 금액에 따라 적절한 배지를 반환
    금액이 큰 배지부터 순회하며, 혜택 금액이 minAmount보다 크다면 그 배지를 발급하는 로직을 구현한다.

--- src/main/java/christmas/domain/promotion/EventBadge.java
+++ src/main/java/christmas/domain/promotion/EventBadge.java
@@ -1,5 +1,7 @@
 package christmas.domain.promotion;
 
+import java.util.Arrays;
+
 public enum EventBadge {
     SANTA("산타", 20_000),
     TREE("트리", 10_000),
@@ -12,5 +14,12 @@ public enum EventBadge {
     EventBadge(String name, int minAmount) {
         this.name = name;
         this.minAmount = minAmount;
+    }
+
+    public static EventBadge findByTotalBenefits(int benefits) {
+        return Arrays.stream(values())
+                .filter(badge -> benefits >= badge.minAmount)
+                .findFirst()
+                .orElse(NONE);
     }
 }
