34d231e28bae40eba6c8eaf4dd88573051c6eed3
MingyeomKim <mingyum119@naver.com>
2023-11-15 21:33:02 +0900

    docs(Date): Date 도메인에 대한 Javadoc 작성

--- src/main/java/christmas/domain/customer/Date.java
+++ src/main/java/christmas/domain/customer/Date.java
@@ -19,14 +19,17 @@ public class Date {
         this.date = date;
     }
 
-    public int getDate() {
-        return date;
-    }
-
     public static Date from(int date) {
         return new Date(date);
     }
 
+    /**
+     * 방문 날짜가 특정 기간 내에 있는지 확인하는 메서드
+     *
+     * @param startDate 시작 날짜
+     * @param endDate   종료 날짜
+     * @return 방문 날짜가 시작 날짜와 종료 날짜 사이의 기간에 속하면 true, 그렇지 않으면 false
+     */
     public boolean isInRange(int startDate, int endDate) {
         return isGreaterThanOrEqualTo(startDate) && isLessThanOrEqualTo(endDate);
     }
@@ -39,25 +42,52 @@ public class Date {
         return this.date <= date;
     }
 
+    /**
+     * 방문 날짜의 크리스마스 디데이 할인 이벤트의 할인 가격을 계산하는 메서드
+     *
+     * @param increment 하루마다 오르는 할인 가격의 증가폭
+     * @return 방문 날짜의 할인 가격
+     */
     public int calculateTotalIncrement(int increment) {
         return (this.date - 1) * increment;
     }
 
+    /**
+     * 방문 날짜가 평일인지 확인하는 메서드
+     *
+     * @return 방문 날짜가 평일이면 true, 그렇지 않으면 false
+     */
     public boolean isWeekday() {
         return DayType.contains(findDayOfWeek(date), DayType.WEEKDAY);
     }
 
+    /**
+     * 방문 날짜가 주말인지 확인하는 메서드
+     *
+     * @return 방문 날짜가 주말이면 true, 그렇지 않으면 false
+     */
     public boolean isWeekend() {
         return DayType.contains(findDayOfWeek(date), DayType.WEEKEND);
     }
 
-    public DayOfWeek findDayOfWeek(int date) {
+
+    private DayOfWeek findDayOfWeek(int date) {
         LocalDate localDate = LocalDate.of(YEAR, MONTH, date);
         return localDate.getDayOfWeek();
     }
 
+    /**
+     * 방문 날짜의 동등성을 확인하는 메서드
+     *
+     * @param date 동등성을 비교할 날짜
+     * @return 날짜가 동등하면 true, 그렇지 않으면 false
+     */
     public boolean equals(int date) {
         return this.date == date;
+    }
+
+    public int getDate() {
+        return date;
     }
 
     private static class Validator {
