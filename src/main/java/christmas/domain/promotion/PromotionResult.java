38c9484d2421b6cd42254f917161c419fcc5dfcf
MingyeomKim <mingyum119@naver.com>
2023-11-15 22:11:18 +0900

    docs(PromotionResult): PromotionResult 메서드에 대한 Javadoc 작성

--- src/main/java/christmas/domain/promotion/PromotionResult.java
+++ src/main/java/christmas/domain/promotion/PromotionResult.java
@@ -28,14 +28,30 @@ public class PromotionResult<T extends Benefit> {
         );
     }
 
+    /**
+     * 혜택 종류를 확인하는 메서드
+     *
+     * @param clazz 혜택 종류를 저장하는 클래스
+     * @return 입력된 혜택 종류에 해당하면 true, 그렇지 않으면 false
+     */
     public boolean isInstanceOf(Class<?> clazz) {
         return clazz.isInstance(benefit);
     }
 
+    /**
+     * 증정 혜택 객체를 다운 캐스팅하여 반환하는 메서드
+     *
+     * @return 증정 혜택 객체
+     */
     public Gift getGift() {
         return (Gift) benefit;
     }
 
+    /**
+     * 할인 혜택 객체를 다운 캐스팅하여 반환하는 메서드
+     *
+     * @return 할인 혜택 객체
+     */
     public Discount getDiscount() {
         return (Discount) benefit;
     }
