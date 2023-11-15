8a84c5a3baf8c26c49804f40f6ae87ef229c039a
MingyeomKim <mingyum119@naver.com>
2023-11-15 13:27:20 +0900

    dosc(EventBadge): 이벤트 배지 클래스에 대한 Javadoc 작성

--- src/main/java/christmas/domain/promotion/EventBadge.java
+++ src/main/java/christmas/domain/promotion/EventBadge.java
@@ -2,6 +2,9 @@ package christmas.domain.promotion;
 
 import java.util.Arrays;
 
+/**
+ * 이벤트 배지의 이름과 배지를 부여하는 최소 기준 금액을 저장하는 클래스
+ */
 public enum EventBadge {
     SANTA("산타", 20_000),
     TREE("트리", 10_000),
@@ -16,6 +19,12 @@ public enum EventBadge {
         this.minAmount = minAmount;
     }
 
+    /**
+     * 금액이 큰 배지부터 순회하며, 혜택 금액이 배지의 기준 금액보다 크다면 바로 발급하는 메서드
+     *
+     * @param benefits 총 혜택 금액
+     * @return 이벤트 배지
+     */
     public static EventBadge findByTotalBenefits(int benefits) {
         return Arrays.stream(values())
                 .filter(badge -> benefits >= badge.minAmount)
