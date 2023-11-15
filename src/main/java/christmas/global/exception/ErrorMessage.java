2d33bfda9fefc5f57406d9355a836e96e8953eba
MingyeomKim <mingyum119@naver.com>
2023-11-15 18:51:48 +0900

    refactor(ErrorMessage): 에러 메세지의 오타 수정

--- src/main/java/christmas/global/exception/ErrorMessage.java
+++ src/main/java/christmas/global/exception/ErrorMessage.java
@@ -2,8 +2,8 @@ package christmas.global.exception;
 
 public enum ErrorMessage {
     BLANK_INPUT_ERROR("빈 문자열이 입력되었습니다."),
-    INVALID_DATE_ERROR("유효하지 않은 날짜입니다. 다시 입력해주세요."),
-    INVALID_ORDER_ERROR("유효하지 않은 주문입니다. 다시 입력해주세요.");
+    INVALID_DATE_ERROR("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    INVALID_ORDER_ERROR("유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
     private final String message;
 
