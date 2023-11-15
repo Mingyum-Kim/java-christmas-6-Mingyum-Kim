26ddc285a5839eddb51bb5cde9b6f4c882c43c67
MingyeomKim <mingyum119@naver.com>
2023-11-15 19:26:20 +0900

    docs(Order): 메서드 선언 순서 컨벤션 적용

--- src/main/java/christmas/domain/customer/Order.java
+++ src/main/java/christmas/domain/customer/Order.java
@@ -17,20 +17,8 @@ public class Order {
         return new Order(name, count);
     }
 
-    public MenuItem getMenuItem() {
-        return menuItem;
-    }
-
-    public Count getCount() {
-        return count;
-    }
-
     public OrderResponse toResponse() {
         return new OrderResponse(menuItem.getName(), count.getValue());
-    }
-
-    public int calculateOrderCost() {
-        return menuItem.getCost() * count.getValue();
     }
 
     /**
@@ -43,11 +31,40 @@ public class Order {
         return Menu.contains(menu, menuItem);
     }
 
+    /**
+     * 현재 메뉴의 개수가 할인 가격보다 작은 지 확인하는 메서드
+     *
+     * @param discountPrice 메뉴 당 할인 가격
+     * @return 메뉴의 가격이 할인 가겨보다 작으면 true, 크거나 같으면 false
+     */
     public boolean isLessThan(int discountPrice) {
         return menuItem.getCost() < discountPrice;
     }
 
+    /**
+     * 특정 메뉴 주문에 대한 금액을 계산하는 메서드
+     *
+     * @return 해당 메뉴에 대한 금액
+     */
+    public int calculateOrderCost() {
+        return menuItem.getCost() * count.getValue();
+    }
+
+    /**
+     * 메뉴의 주문 개수와 입력된 가격을 곱하는 메서드
+     *
+     * @param price 가격
+     * @return 메뉴의 개수만큼 가격을 곱한 결과값
+     */
     public int calculateTotalPrice(int price) {
         return price * count.getValue();
+    }
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+
+    public Count getCount() {
+        return count;
     }
 }
